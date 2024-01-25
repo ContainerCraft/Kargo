fba491002e68bef10c79cb3cfe7b4b6dc9175b21
Kat Morgan <kat@braincraft.io>
2024-01-25 23:30:17 +0000

    clean end to end manual test

--- __main__.py
+++ __main__.py
@@ -3,11 +3,8 @@ import os
 import pulumi_kubernetes as kubernetes
 from pulumi_kubernetes import Provider, core, helm, apps
 
-# lazily setting variables for now
-cluster_ip = "192.168.1.41"
-
 config = pulumi.Config()
-context = config.get_secret("kubeconfig.context") or "admin@talos-default"
+context = config.get("kubeconfig.context") or "admin@talos-default"
 k8s_provider = kubernetes.Provider("k8sProvider", kubeconfig=pulumi.Output.secret(os.getenv("KUBECONFIG")), context=context)
 
 class KubernetesPlatformProject:
@@ -25,14 +22,13 @@ class KubernetesPlatformProject:
         )
 
 kubernetes_platform_project = KubernetesPlatformProject("kargo")
-
 pulumi.export('namespace_name', kubernetes_platform_project.namespace.metadata.name)
 
 # Using Helm Values per Talos Docs
 # - https://www.talos.dev/latest/kubernetes-guides/network/deploying-cilium/#method-1-helm-install
 cilium_helm_values = {
     "cluster": {
-      "name": "kargo.dev"
+        "name": "kargo.dev"
     },
     "cgroup": {
         "autoMount": {"enabled": False},
@@ -83,19 +79,22 @@ cilium_helm_values = {
     },
 }
 
-cilium_helm_release = helm.v3.Release("cilium-release",
-                                      chart="cilium",
-                                      name="cilium",
-                                      repository_opts={"repo": "https://helm.cilium.io/"},
-                                      version="1.14.5",
-                                      values=cilium_helm_values,
-                                      namespace=cilium_helm_values["namespace"],
-                                      wait_for_jobs=True,
-                                      skip_await=False,
-                                      skip_crds=False,
-                                      lint=True,
-                                      opts=pulumi.ResourceOptions(provider=k8s_provider))
+cilium_helm_release = helm.v3.Release(
+    "cilium-release",
+    chart="cilium",
+    name="cilium",
+    repository_opts={"repo": "https://helm.cilium.io/"},
+    version="1.14.5",
+    values=cilium_helm_values,
+    namespace=cilium_helm_values["namespace"],
+    wait_for_jobs=True,
+    skip_await=False,
+    skip_crds=False,
+    lint=True,
+    opts=pulumi.ResourceOptions(
+        provider=k8s_provider
+    )
+)
 
 pulumi.export("ciliumResources", cilium_helm_release.resource_names)
 pulumi.export("ciliumReleaseName", cilium_helm_release.status.apply(lambda s: s.name))
-pulumi.export("clusterIP", cluster_ip)
