7b0c5f4180f937721bc3a2f6dc5f96f07d05eb4c
Kat Morgan <kat@braincraft.io>
2024-03-10 00:53:58 +0000

    90% working ceph operator deploy, need to debug err

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 import pulumi
+from pulumi import export as export
 import pulumi_kubernetes as k8s
 from src.kargo.cilium import deploy as deploy_cilium
 from src.lib.kubernetes_api_endpoint import KubernetesApiEndpointIp
 from src.lib.namespace import create_namespaces
-from src.kargo.kubevirt import deploy_kubevirt  # Import the KubeVirt deployment function
+from src.kargo.kubevirt import deploy_kubevirt  # Import the KubeVirt deployment
+from src.kargo.ceph.deploy import deploy as deploy_rook_operator  # Import the Rook Ceph deployment
 import os
 
 def main():
@@ -32,7 +34,11 @@ def main():
     kubernetes_endpoint_ip = KubernetesApiEndpointIp("k8s-api-ip", k8s_provider)
 
     # Define desired namespaces
-    namespaces = ["kargo", "kubevirt"]
+    namespaces = [
+        "kargo",
+        "kubevirt",
+        "rook-ceph"
+    ]
 
     # Create namespaces
     namespace_objects = create_namespaces(namespaces, k8s_provider)
@@ -48,16 +54,32 @@ def main():
     )
 
     # Deploy KubeVirt
-    kubevirt_version = deploy_kubevirt(k8s_provider, kubernetes_distribution)
+    kubevirt_version = deploy_kubevirt(
+        k8s_provider,
+        kubernetes_distribution
+    )
+
+    # check if pulumi config ceph.enabled is set to true and deploy rook-ceph if it is
+    deploy_ceph = config.get_bool('ceph.enabled') or False
+    if deploy_ceph:
+        # Deploy Rook Ceph
+        rook_operator = deploy_rook_operator(
+            "kargo",
+            k8s_provider,
+            kubernetes_distribution,
+            "kargo",
+            "rook-ceph"
+        )
+        export('rook_operator', rook_operator)
 
     # Export deployment details
-    pulumi.export('helm_release_name', cilium_helm_release.resource_names)
-    pulumi.export('k8s_provider', k8s_provider)
-    pulumi.export('namespace_names', [ns.metadata.name for ns in namespace_objects])
-    pulumi.export('kubeconfig_context', kubeconfig_context)
-    pulumi.export('kubernetes_distribution', kubernetes_distribution)
-    pulumi.export('kubernetes_endpoint_ips', kubernetes_endpoint_ip.ips)
-    pulumi.export('kubevirt_version', kubevirt_version)
+    export('helm_release_name', cilium_helm_release.resource_names)
+    export('k8s_provider', k8s_provider)
+    export('namespace_names', [ns.metadata.name for ns in namespace_objects])
+    export('kubeconfig_context', kubeconfig_context)
+    export('kubernetes_distribution', kubernetes_distribution)
+    export('kubernetes_endpoint_ips', kubernetes_endpoint_ip.ips)
+    export('kubevirt_version', kubevirt_version)
 
 # Execute the main function
 main()
