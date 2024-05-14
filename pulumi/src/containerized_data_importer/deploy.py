82da8228197d610849900cb4215b0358a68ed0e8
Kat Morgan <kat@braincraft.io>
2024-05-14 04:12:52 +0000

    working cdi custom resource

--- pulumi/src/containerized_data_importer/deploy.py
+++ pulumi/src/containerized_data_importer/deploy.py
@@ -1,7 +1,7 @@
 import requests
 import pulumi
 import pulumi_kubernetes as k8s
-from pulumi_kubernetes.apiextensions import CustomResource
+from pulumi_kubernetes.apiextensions.CustomResource import CustomResource
 from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
 def deploy_cdi(version: str, k8s_provider: k8s.Provider):
@@ -19,22 +19,53 @@ def deploy_cdi(version: str, k8s_provider: k8s.Provider):
 
     # Deploy the CDI operator
     cdi_operator_url = f'https://github.com/kubevirt/containerized-data-importer/releases/download/v{version}/cdi-operator.yaml'
+    pulumi.log.info(f"Deploying CDI operator from: {cdi_operator_url}")
     operator = k8s.yaml.ConfigFile(
         'cdi-operator',
         file=cdi_operator_url,
-        opts=pulumi.ResourceOptions(provider=k8s_provider)
+        opts=pulumi.ResourceOptions(
+            provider=k8s_provider
+        )
     )
 
-    ## Deploy the default CDI custom resource
-    #cdi_cr_url = f'https://github.com/kubevirt/containerized-data-importer/releases/download/v{version}/cdi-cr.yaml'
-    #cdi_cr = k8s.yaml.ConfigFile(
-    #    'cdi-cr',
-    #    file=cdi_cr_url,
-    #    opts=pulumi.ResourceOptions(
-    #        provider=k8s_provider,
-    #        #parent=namespace,
-    #        depends_on=[operator]
-    #    )
-    #)
+    # Deploy the default CDI custom resource
+    cdi_resource = CustomResource(
+        "cdi",
+        api_version="cdi.kubevirt.io/v1beta1",
+        kind="CDI",
+        metadata={
+            "name": "cdi",
+            "namespace": "cdi",
+        },
+        spec={
+            "config": {
+                "featureGates": [
+                    "HonorWaitForFirstConsumer",
+                ],
+            },
+            "imagePullPolicy": "IfNotPresent",
+            "infra": {
+                "nodeSelector": {
+                    "kubernetes.io/os": "linux",
+                },
+                "tolerations": [
+                    {
+                        "key": "CriticalAddonsOnly",
+                        "operator": "Exists",
+                    },
+                ],
+            },
+            "workload": {
+                "nodeSelector": {
+                    "kubernetes.io/os": "linux",
+                },
+            },
+        },
+        opts=pulumi.ResourceOptions(
+            provider=k8s_provider,
+            depends_on=[operator]
+            #parent=namespace
+        )
+    )
 
     return version, operator
