084dd33d9cd3407373318a4c2176883fd6e54361
Kat Morgan <kat@braincraft.io>
2024-06-10 05:10:01 +0000

    wip creating kargo on kargo dev environment

--- metal/dev/apply.sh
+++ metal/dev/apply.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -x
 #talosctl gen secrets
 #talosctl gen config dev https://192.168.1.60:6443/ \
-#--install-image "factory.talos.dev/installer/d1cba4b30888a004f2da2ec088afa62af8bf97b049367afec139490bec401a73:v1.7.2" \
+#--install-image "factory.talos.dev/installer/b027a2d9dddfa5c0752c249cf3194bb5c62294dc7cba591f3bec8119ab578aea:v1.7.4" \
 #--output-types controlplane,worker,talosconfig \
 #--with-secrets secrets.yaml \
-#--config-patch @tmpl.yaml \
+#--config-patch @template.yaml \
 #--install-disk "/dev/vda" \
 #--output ./ \
 #--persist \
 #--force
 
-kubectl delete -f kubevirt.vm.yaml --wait=true ; sleep 3
+kubectl delete -f kubevirt.vm.yaml --wait=true ; sleep 1 ; kubectl delete -f kubevirt.vm.yaml --wait=true ; sleep 3
 kubectl delete secret talos-userdata-cp1 ; sleep 1
+
 kubectl create secret generic talos-userdata-cp1 --dry-run=client --output=yaml \
 --from-file=userdata=controlplane.yaml \
 | kubectl apply -f -
-kubectl apply -f kubevirt.vm.yaml
+
+
+kubectl apply -f kubevirt.vm.yaml || kubectl apply -f kubevirt.vm.yaml
